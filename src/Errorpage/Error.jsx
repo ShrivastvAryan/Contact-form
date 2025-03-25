1ff908d4c0ab24697a7634054b4618b17f8a46d3
Aryan Shrivastava <asguy29@gmail.com>
2025-03-25 22:47:32 +0530

    added contact form

--- src/Errorpage/Error.jsx
+++ src/Errorpage/Error.jsx
@@ -3,4 +3,14 @@
 <Route path="*" element={<Error/>}/> that path to * ensures that any other page will show that error 404
 </Routes>*/ 
 
-et Error=
+export const Error=()=>{
+    return(
+        <>
+        <section>
+            <div>
+                <h1>Error 404 page not found</h1>  
+            </div>
+        </section>
+        </>
+    )
+}
